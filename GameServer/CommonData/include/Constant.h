f9683eec9c36aeb30c1a9287880c0b281e31398f
a83376750 <394315001@qq.com>
2016-07-24 22:22:19 +0800

    添加公共项目

---
+++ GameServer/CommonData/include/Constant.h
@@ -0,0 +1,15 @@
+#pragma once
+
+enum ERRID
+{
+	StartSocket_ERR = 1000,
+	CreateSocket_ERR,				//1001
+	BindSocket_ERR,					//1002
+	ListenSocket_ERR,				//1003
+	AcceptSocket_ERR,				//1004
+	ConnectSocket_ERR,				//1005
+	RecvSocket_ERR,					//1006
+	SendSocket_ERR,					//1007
+};
+
+#define PRINTFERRORINFO(ID) printf("Error ID:%d\n",ID)
