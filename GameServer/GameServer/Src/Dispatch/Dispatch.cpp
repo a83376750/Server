46468ea0a275ae1a5c4e2c6dd38eeec03793273d
a83376750 <394315001@qq.com>
2016-07-27 23:25:47 +0800

    添加新定义结构

---
+++ GameServer/GameServer/Src/Dispatch/Dispatch.cpp
@@ -0,0 +1,28 @@
+#include "Dispatch.h"
+
+
+
+CDispatch::CDispatch()
+{
+}
+
+
+CDispatch::~CDispatch()
+{
+}
+
+
+CServerDispatch::~CServerDispatch()
+{
+
+}
+
+unsigned int CServerDispatch::Dis_Send(void *Target, void *Source)
+{
+
+}
+
+unsigned int CServerDispatch::Dis_Recv(void *Target, void *Source)
+{
+
+}
