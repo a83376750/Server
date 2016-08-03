a086134d3c9d252c7843c166c75a40fb740712b8
a83376750 <394315001@qq.com>
2016-08-03 16:32:03 +0800

    补充

--- GameServer/GameServer/Src/Dispatch/Dispatch.cpp
+++ GameServer/GameServer/Src/Dispatch/Dispatch.cpp
@@ -19,10 +19,10 @@ CServerDispatch::~CServerDispatch()
 
 unsigned int CServerDispatch::Dis_Send(void *Target, void *Source)
 {
-
+	return 0;
 }
 
 unsigned int CServerDispatch::Dis_Recv(void *Target, void *Source)
 {
-
+	return 0;
 }
